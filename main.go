96b9c3824d3bcd525df833e399747a23628f087e
Anders Pitman <tapitman11@gmail.com>
2020-05-13 16:16:08 -0600

    Implement email alerts

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+        "fmt"
 	"net/http"
+        "net/smtp"
         "log"
         "encoding/json"
         "io/ioutil"
@@ -10,6 +12,18 @@ import (
         "flag"
         "path"
 )
+
+type Config struct {
+        Smtp *SmtpConfig
+}
+
+type SmtpConfig struct {
+        Server string `json:"server"`
+        Port int `json:"port"`
+        Username string `json:"username"`
+        Password string `json:"password"`
+        Sender string `json:"sender"`
+}
 
 type ChecksConfig struct {
         Checks []*CheckConfig `json:"checks"`
@@ -20,6 +34,7 @@ type CheckConfig struct {
         CheckCommand string `json:"check_command"`
         SuccessCommand string `json:"success_command"`
         FailCommand string `json:"fail_command"`
+        AlertEmail string  `json:"alert_email"`
 }
 
 func main() {
@@ -28,6 +43,20 @@ func main() {
         dir := flag.String("dir", "./", "Directory")
         flag.Parse()
 
+        configFilePath := path.Join(*dir, "config.json")
+        configFile, err := ioutil.ReadFile(configFilePath)
+        if err != nil {
+                log.Fatal(err)
+                return
+        }
+
+        config := &Config{}
+        err = json.Unmarshal(configFile, config)
+        if err != nil {
+                log.Fatal(err)
+                return
+        }
+
         checksFilePath := path.Join(*dir, "checks.json")
         checksFile, err := ioutil.ReadFile(checksFilePath)
         if err != nil {
@@ -35,17 +64,17 @@ func main() {
                 return
         }
 
-        config := &ChecksConfig{}
-        err = json.Unmarshal(checksFile, config)
+        checksConfig := &ChecksConfig{}
+        err = json.Unmarshal(checksFile, checksConfig)
         if err != nil {
                 log.Fatal(err)
                 return
         }
 
-        validateConfig(config)
+        validateChecks(checksConfig)
 
-        for _, check := range config.Checks {
-                go startJob(check)
+        for _, check := range checksConfig.Checks {
+                go startJob(config, check)
         }
 
 	handler := func(w http.ResponseWriter, r *http.Request) {
@@ -59,25 +88,45 @@ func main() {
 }
 
 
-func startJob(check *CheckConfig) {
+func startJob(config *Config, check *CheckConfig) {
         for {
                 command := exec.Command("bash", "-c", check.CheckCommand)
                 _, err := command.Output()
                 if err != nil {
                         log.Println("Check failed:", check.CheckCommand)
                         go runFailCommand(check)
+
+                        if len(check.AlertEmail) > 0 {
+                                sendEmail(config.Smtp, check.AlertEmail, check.CheckCommand)
+                        }
                 }
 
                 time.Sleep(time.Duration(check.IntervalMS) * time.Millisecond)
         }
 }
 
-func validateConfig(config *ChecksConfig) {
-        for _, check := range config.Checks {
+func validateChecks(checksConfig *ChecksConfig) {
+        for _, check := range checksConfig.Checks {
                 if check.IntervalMS < 100 {
                         log.Fatal("Min 100 MS interval")
                 }
         }
+}
+
+func sendEmail(smtpConfig *SmtpConfig, email string, command string) {
+        auth := smtp.PlainAuth("", smtpConfig.Username, smtpConfig.Password, smtpConfig.Server)
+        to := []string{email}
+	msg := []byte(fmt.Sprintf("To: %s\r\n", email) +
+                fmt.Sprintf("From: stealthcheck automated alerts <%s>\r\n", smtpConfig.Sender) +
+		"Subject: stealthcheck command failed\r\n" +
+		"\r\n" +
+                "The following command failed: '" + command + "'\r\n")
+        addr := fmt.Sprintf("%s:%d", smtpConfig.Server, smtpConfig.Port)
+        err := smtp.SendMail(addr, auth, smtpConfig.Sender, to, msg)
+	if err != nil {
+                fmt.Println(err)
+                log.Println("Sending alert email failed: " + email)
+	}
 }
 
 func runFailCommand(check *CheckConfig) {
